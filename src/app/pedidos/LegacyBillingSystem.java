8788c8e8eb802a3426a5f32e0308fe6b348d5ce7
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 01:58:02 -0500

    Se creo el Metodo LegacyBillingSystem

--- src/app/pedidos/LegacyBillingSystem.java
+++ src/app/pedidos/LegacyBillingSystem.java
@@ -9,5 +9,9 @@ package app.pedidos;
  * @author yusuk
  */
 public class LegacyBillingSystem {
+
+    void createInvoice(String cliente, double total) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
