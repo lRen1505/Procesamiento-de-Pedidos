a68ba925a983cd1754c4b778d8889fab265a8ffa
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:17:17 -0500

    Se borraron comentarios innecesarios y funciones no requeridas

--- src/app/pedidos/CalculoService.java
+++ src/app/pedidos/CalculoService.java
@@ -1,25 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app.pedidos;
 
-/**
- *
- * @author yusuk
- */
+
 public class CalculoService {
 
     double calcularSubtotal(String producto, int cantidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     double calcularIGV(double subtotal) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+     
     }
 
     double calcularTotal(double subtotal, double igv) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
     }
     
 }
