149011cecc64ef59ff0e5f464c9582678fc113ba
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:27:46 -0500

    importamos de la bibliteca la fecha y hora , localDateTime y DateTimeFormatter

--- src/app/pedidos/ComprobanteService.java
+++ src/app/pedidos/ComprobanteService.java
@@ -1,11 +1,16 @@
 
 package app.pedidos;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class ComprobanteService {
 
-    String generarComprobante(String cliente, String producto, double subtotal, double igv, double total) {
-       
+ public String generarComprobante(String cliente, String producto, double subtotal, double igv, double total) {
+      LocalDateTime ahora = LocalDateTime.now();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+      String fechaHora = ahora.format(formatter); 
     }
     
 }
