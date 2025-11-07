adde45ac27d8330558ff3c8b0747a8be29bbda2c
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:46:30 -0500

    He creado el sistema facade, legacy y sistema legacy

--- src/app/pedidos/Main.java
+++ src/app/pedidos/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-      
+      System.out.println("=== SISTEMA DE PROCESAMIENTO DE PEDIDOS ===\n");
     }
-    
+   LegacyBillingSystem legacySystem = new LegacyBillingSystem();
+   FacturaService facturaService = new FacturaAdapter(legacySystem);
+   PedidoFacade pedidoFacade = new PedidoFacade(facturaService);
+   
 }
