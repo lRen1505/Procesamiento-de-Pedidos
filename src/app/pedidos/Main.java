a78d81d57d3fe41a57eccfbadf9cd285c9fe969b
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:56:33 -0500

    Corregi el main , habia cerrado el maion del metodo antes de crear los objetos y por eso no podia llamar a procesar pedido

--- src/app/pedidos/Main.java
+++ src/app/pedidos/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
       System.out.println("=== SISTEMA DE PROCESAMIENTO DE PEDIDOS ===\n");
-    }
+    
    LegacyBillingSystem legacySystem = new LegacyBillingSystem();
    FacturaService facturaService = new FacturaAdapter(legacySystem);
    PedidoFacade pedidoFacade = new PedidoFacade(facturaService);
@@ -17,5 +17,6 @@ public class Main {
             "Juan Pérez", 
             "Laptop HP", 
             2
-        );
-}}
+       );
+    }
+}
