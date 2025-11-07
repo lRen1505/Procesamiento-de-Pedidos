0503a80fc16e22b76819c95c2f8086248f25dc7d
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:25:53 -0500

    Se elimina comentarios inncesarios

--- src/app/pedidos/ComprobanteService.java
+++ src/app/pedidos/ComprobanteService.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app.pedidos;
 
-/**
- *
- * @author yusuk
- */
+
 public class ComprobanteService {
 
     String generarComprobante(String cliente, String producto, double subtotal, double igv, double total) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
     
 }
