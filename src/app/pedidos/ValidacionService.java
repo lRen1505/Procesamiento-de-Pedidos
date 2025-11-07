073fd46f5fff270679add4c97df7feee3a508c31
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:15:46 -0500

    En el metodo la cantidad se valida si es OK o si la cantidad es incorrecta

--- src/app/pedidos/ValidacionService.java
+++ src/app/pedidos/ValidacionService.java
@@ -4,8 +4,18 @@ package app.pedidos;
 
 public class ValidacionService {
 
-    boolean validarCantidad(int cantidad) {
+ public boolean validarCantidad(int cantidad) {
+       
+    System.out.println(" Verificando cantidad: " + cantidad);
         
+        if (cantidad > 0) {
+            System.out.println("Cantidad válida: OK");
+            return true;
+        } else {
+            System.out.println("Cantidad inválida: CANTIDAD INCORRECTA");
+            return false;
+        }
+         
     }
 
  public boolean validarStock(String producto, int cantidad) {
