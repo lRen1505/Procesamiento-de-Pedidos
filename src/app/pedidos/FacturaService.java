25211cff0183f594fac99b6c7313259c5da197cc
lRen1505 <renso.escuadra@usil.pe>
2025-11-06 20:57:55 -0500

    Elimine comentarios innecesarios

--- src/app/pedidos/FacturaService.java
+++ src/app/pedidos/FacturaService.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app.pedidos;
 
-/**
- *
- * @author yusuk
- */
+
 public interface FacturaService {
     
     void generarFactura(String cliente, double total);
